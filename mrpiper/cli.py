94fbf3cb1c30645b17be2771108f8d561e92620b
james pacileo <jamespaxi@gmail.com>
2017-09-11 20:54:47 +0100

    fixes

--- mrpiper/cli.py
+++ mrpiper/cli.py
@@ -1,36 +1,43 @@
 import click
 
+@click.group()
+def cli():
+    pass
 
-@click.command()
+@cli.command()
 def outdated():
     "Lists packages that have updates available"
     pass
 
-@click.command()
+@cli.command()
 @click.argument("package_names", nargs=-1)
 def add(package_names):
+    "Install and add a package to requirements"
     pass
 
-@click.command()
+@cli.command()
 @click.argument("package_names", nargs=-1)
 def remove(package_names):
+    "Remove a list of packages and their dependencies, and remove this from the requirements."
     pass
 
-@click.command()
-def install():
+@cli.command()
+@click.option("--dev", default=False, help="If to install dev packages")
+def install(dev):
+    "Install all packages in requirement files."
     pass
 
-@click.command()
+@cli.command()
 def init():
+    "Initialise project with virtual environment, requirements structure and package lock."
     click.echo("Initializing project")
     pass
 
-@click.command()
+@cli.command()
 @click.argument("package_names", nargs=-1)
 def upgrade():
+    "Upgrade a list of packages."
     pass
-
-@click.command()
-def hello():
-    click.echo("Hello world!")
     
+if __name__ == '__main__':
+    cli()
