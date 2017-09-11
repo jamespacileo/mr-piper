fd02cab56e0d80c08611763db3697e270709abe2
james pacileo <jamespaxi@gmail.com>
2017-09-11 21:07:33 +0100

    fixes

--- mrpiper/cli.py
+++ mrpiper/cli.py
@@ -1,4 +1,7 @@
 import click
+import os
+
+import piper
 
 @click.group()
 def cli():
@@ -7,37 +10,42 @@ def cli():
 @cli.command()
 def outdated():
     "Lists packages that have updates available"
-    pass
+    piper.outdated()
 
 @cli.command()
+@click.option("--dev", is_flag=True)
 @click.argument("package_names", nargs=-1)
-def add(package_names):
+def add(dev, package_names):
     "Install and add a package to requirements"
-    pass
+    for package_name in package_names:
+        piper.add(package_name, dev=dev)
 
 @cli.command()
+@click.option("--dev", is_flag=True)
 @click.argument("package_names", nargs=-1)
 def remove(package_names):
     "Remove a list of packages and their dependencies, and remove this from the requirements."
-    pass
+    for package_name in package_names:
+        piper.remove(package_name, dev=dev)
 
 @cli.command()
-@click.option("--dev", default=False, help="If to install dev packages")
+@click.option("--dev", is_flag=True, help="If to install dev packages")
 def install(dev):
     "Install all packages in requirement files."
-    pass
+    piper.install()
 
 @cli.command()
 def init():
     "Initialise project with virtual environment, requirements structure and package lock."
     click.echo("Initializing project")
-    pass
+    piper.init()
 
 @cli.command()
 @click.argument("package_names", nargs=-1)
 def upgrade():
     "Upgrade a list of packages."
-    pass
+    piper.upgrade()
     
 if __name__ == '__main__':
+    os.chdir("..")
     cli()
