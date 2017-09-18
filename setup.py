dd7feb176103f1a2fe0e0a5c0ae797a628e8fd9d
james pacileo <jamespaxi@gmail.com>
2017-09-18 17:14:46 +0100

    fixes

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='mrpiper',
     version='0.1',
+    license="MIT",
     py_modules=['mrpiper.cli'],
+    author='James Pacileo',
     packages=find_packages(),
     install_requires=[
         'pytest',
@@ -21,4 +23,16 @@ setup(
         [console_scripts]
         piper=mrpiper.cli:cli
     ''',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
 )
