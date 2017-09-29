2fcdfe9fa66ea9bbbe5dc403f0dcbd5fd316e1d2
james pacileo <jamespaxi@gmail.com>
2017-09-29 14:46:51 +0100

    better docs

--- mrpiper/cli.py
+++ mrpiper/cli.py
@@ -18,15 +18,16 @@ from . import piper
 
 @click.group()
 def cli():
-    """                                                                             |
-     _____ _                                                                        |
-    |  __ (_)                                                                       |
-    | |__) | _ __   ___ _ __                                                        |
-    |  ___/ | '_ \ / _ \ '__|                                                       |
-    | |   | | |_) |  __/ |                                                          |
-    |_|   |_| .__/ \___|_|                                                          |
-            | |                                                                     |
-            |_|                                                                     |
+    """
+    \b
+     _____ _
+    |  __ (_)
+    | |__) | _ __   ___ _ __
+    |  ___/ | '_ \ / _ \ '__|
+    | |   | | |_) |  __/ |
+    |_|   |_| .__/ \___|_|
+            | |
+            |_|
     """
 
 # @cli.command()
@@ -37,36 +38,6 @@ def cli():
 #     for package_name in package_names:
 #         piper.add(package_name, dev=dev)
 #         click.echo("")
-
-@cli.command()
-@click.option("--dev", is_flag=True)
-@click.option("--editable", "-e", is_flag=True)
-@click.argument("package_names", nargs=-1)
-@click_log.simple_verbosity_option(logger)
-def add(dev, editable, package_names):
-    "Install and add a package to requirements"
-    for package_name in package_names:
-        piper.add(package_name, editable=editable, dev=dev)
-        # click.echo("")
-
-@cli.command()
-@click.option("--noinput", is_flag=True)
-@click.argument("package_names", nargs=-1)
-@click_log.simple_verbosity_option(logger)
-def remove(noinput, package_names):
-    "Remove a list of packages and their dependencies, and remove this from the requirements."
-    for package_name in package_names:
-        piper.remove(package_name)
-        click.echo("")
-
-@cli.command()
-@click.option("--dev", is_flag=True, help="If to install dev packages")
-@click.option("--cache", "cache_url", default=None, help="Cache url to grab packages from")
-@click.option("--hashes", "require_hashes", is_flag=True, help="Should install check the hashes")
-@click_log.simple_verbosity_option(logger)
-def install(dev, cache_url, require_hashes):
-    "Install all packages in requirement files."
-    piper.install(dev=dev, cache_url=cache_url, require_hashes=require_hashes)
 
 @cli.command()
 @click.option("--import", "-r", "file_to_import", help="Import from existing requirements file")
@@ -80,8 +51,77 @@ def install(dev, cache_url, require_hashes):
 @click.option("--installable", "installable", is_flag=True, help="Use installable Python")
 @click_log.simple_verbosity_option(logger)
 def init(file_to_import, virtualenv_location, noinput, private, python, is_global, installable):
-    "Initialise project with virtual environment, requirements structure and package lock."
+    """
+    \b
+    Initialises the project with:
+    - a Piper project file (piper.json)
+    - a Piper lock file (piper.lock)
+    - legacy PIP requirements (base.txt, base-locked.txt, dev.txt, dev-locked.txt)
+    - (optional) a pre-configured setup.py
+
+    Examples:
+
+    > piper init
+    > piper init --py 3.6
+    > piper init -y
+
+    """
     piper.init(noinput=noinput, python=python, virtualenv_location=virtualenv_location, installable=installable)
+
+
+
+@cli.command()
+@click.option("--dev", is_flag=True)
+@click.option("--editable", "-e", is_flag=True)
+@click.argument("package_names", nargs=-1)
+@click_log.simple_verbosity_option(logger)
+def add(dev, editable, package_names):
+    """
+    Install and add a package to requirements
+
+    \b
+    Examples:
+    $ piper add django
+    $ piper add pytest --dev
+    $ piper add -e requests/requests
+    """
+    for package_name in package_names:
+        piper.add(package_name, editable=editable, dev=dev)
+        # click.echo("")
+
+@cli.command()
+@click.option("--noinput", is_flag=True)
+@click.argument("package_names", nargs=-1)
+@click_log.simple_verbosity_option(logger)
+def remove(noinput, package_names):
+    """
+    Remove a list of packages and their dependencies, and remove this from the requirements.
+
+    \b
+    Example:
+    $ piper remove django --noinput
+    """
+    for package_name in package_names:
+        piper.remove(package_name)
+        click.echo("")
+
+@cli.command()
+@click.option("--dev", is_flag=True, help="If to install dev packages")
+@click.option("--cache", "cache_url", default=None, help="Cache url to grab packages from")
+@click.option("--hashes", "require_hashes", is_flag=True, help="Should install check the hashes")
+@click_log.simple_verbosity_option(logger)
+def install(dev, cache_url, require_hashes):
+    """
+    Install all packages in requirement files.
+
+    \b
+    Examples:
+    $ piper install
+    $ piper install --dev
+    $ piper install --hashes
+    """
+    piper.install(dev=dev, cache_url=cache_url, require_hashes=require_hashes)
+
 
 @cli.command()
 @click.option("--patch", "upgrade_level", flag_value="patch", help="For patch version upgrades")
@@ -93,7 +133,15 @@ def init(file_to_import, virtualenv_location, noinput, private, python, is_globa
 @click.argument("package_names", nargs=-1)
 @click_log.simple_verbosity_option(logger)
 def upgrade(upgrade_level, all, noinput, package_names):
-    "Upgrade a list of packages."
+    """
+    Upgrade a list of packages.
+
+    \b
+    Examples:
+    $ piper upgrade django --latest
+    $ piper upgrade requests --patch
+    $ piper upgrade --all --yes
+    """
     # click.echo([patch, minor, major, latest, package_names])
 
     if all:
@@ -110,7 +158,14 @@ def upgrade(upgrade_level, all, noinput, package_names):
 @click.option("--verbose", is_flag=True, help="Show some extra columns")
 @click_log.simple_verbosity_option(logger)
 def outdated(output_format, all_pkgs, verbose):
-    "Deletes virtualenv, requirements folder/files and piper file."
+    """
+    Deletes virtualenv, requirements folder/files and piper file.
+
+    \b
+    Example:
+    $ piper outdated --all --json
+
+    """
     # logger.error("Test message")
     # logger.debug("{0} {1} {2}".format(output_format, all_pkgs, verbose))
     piper.outdated(output_format=output_format, all_pkgs=all_pkgs, verbose=verbose)
@@ -125,14 +180,28 @@ def wipe(noinput):
 @click.argument("package_name") #, help="Package name you want to explain why")
 @click_log.simple_verbosity_option(logger)
 def why(package_name):
-    "Explain why a package exists"
+    """
+    Explain why a package exists
+
+    \b
+    Example:
+    $ piper why django
+
+    """
     piper.why(package_name)
 
 
 @cli.command()
 @click_log.simple_verbosity_option(logger)
 def list():
-    "List all installed packages"
+    """
+    List all installed packages
+
+    \b
+    Example:
+    $ piper list
+
+    """
     piper.dependency_list()
 
 
@@ -140,20 +209,40 @@ def list():
 @click.option("--yes", "-y", "noinput", is_flag=True)
 @click_log.simple_verbosity_option(logger)
 def version(noinput):
-    "Get and set project version"
+    """
+    Get and set project version
+
+    \b
+    Example:
+    $ piper version -y
+
+    """
     piper.version(noinput)
 
 @cli.command()
 @click.argument("package_name", nargs=1)
 @click_log.simple_verbosity_option(logger)
 def info(package_name):
-    "info all installed packages"
+    """
+    info all installed packages
+
+    \b
+    Example:
+    $ piper info django
+
+    """
     piper.info(package_name)
 
 @cli.command()
 @click_log.simple_verbosity_option(logger)
 def activate():
-    "Echo the virtualenv activation commandline"
+    """
+    Echo the virtualenv activation commandline
+
+    \b
+    Example:
+    $ piper activate
+    """
     piper.activate()
 
 @cli.command()
@@ -161,7 +250,13 @@ def activate():
 @click.option("--dev", "dev", is_flag=True)
 @click_log.simple_verbosity_option(logger)
 def cache(output_dir, dev=False):
-    "Cache your installed packages"
+    """
+    Cache your installed packages
+
+    \b
+    Example:
+    $ piper cache -o /path/to/cache
+    """
     piper.cache(output_dir, dev)
 
 @cli.command()
@@ -173,7 +268,13 @@ def fix():
 @cli.command()
 @click_log.simple_verbosity_option(logger)
 def hash():
-    "Gather dependency hashes"
+    """
+    Gather dependency hashes
+
+    \b
+    Example:
+    $ piper hash
+    """
     piper.hash()
 
 @cli.command()
@@ -181,13 +282,38 @@ def hash():
 @click.argument('args', nargs=-1, type=click.UNPROCESSED)
 @click_log.simple_verbosity_option(logger)
 def run(name, args):
-    "Run a list of scripts sequentially"
+    """
+    Run a list of scripts sequentially
+
+    \b
+    Example:
+    $ piper run build_publish
+    """
     piper.run(name, args)
+
+
+@cli.command()
+@click_log.simple_verbosity_option(logger)
+def integrity():
+    """
+    Run an integrity check and fix any discrepancies in the lock and requirement files
+
+    \b
+    Example:
+    $ piper integrity
+    """
+    piper.check_integrity()
 
 @cli.command()
 @click_log.simple_verbosity_option(logger)
 def shell():
-    "Run a shell within the virtualenv"
+    """
+    Run a shell within the virtualenv
+
+    \b
+    Example:
+    $ piper shell
+    """
     piper.shell()
 
 # @cli.command()
